e5bc8560e469aabd47b8a7e393f219b5aa696a74
daudi13 <davouma13@gmail.com>
2021-09-14 08:11:28 +0300

    commit arrow function

--- index.js
+++ index.js
@@ -14,3 +14,23 @@ function calcAge2(birthYear) {
 }
 const myAgeNow = calcAge2(1994);
 console.log(myAgeNow);
+
+//arrow function 
+
+const theBirthday = birthYear => 2021 - birthYear
+
+const age3 = theBirthday(1988);
+
+console.log(age3)
+
+
+//years left until retirement
+
+const yearsToRetirement = (birthYear, firstName) => {
+    const age = 2021 - birthYear;
+    const retirement = 65 - age;
+    return `${firstName} still has ${retirement} years to leave his blistering career`
+}
+
+const yearsLeft = yearsToRetirement(1994, "Matthew");
+console.log(yearsLeft);
