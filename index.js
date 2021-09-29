b2370650f269d7659a514c0632cf96b0216efd3c
daudi13 <davouma13@gmail.com>
2021-09-29 16:51:24 +0300

    commit sum

--- index.js
+++ index.js
@@ -135,3 +135,41 @@ const total = new Array(tips[0] + bills[0], tips[1] + bills[1], tips[2] + bills[
 
 console.log(total);
 
+
+//coding challenge
+
+const mark = {
+    fullName: 'Mark Miller',
+    mass: 78,
+    height: 1.69,
+    
+    calcBMI: function () {
+        this.BMI = this.mass / this.height ** 2;
+        return this.BMI
+    }
+}
+
+const john = {
+    fullName: 'John Smith',
+    mass: 92,
+    height: 1.95,
+    
+    calcBMI: function () {
+        this.BMI = this.mass / this.height ** 2;
+        return this.BMI;
+    }
+}
+
+
+function compareBMI() {
+    if (mark.calcBMI() > john.calcBMI()) {
+        console.log(`${mark.fullName}'s  BMI(${mark.calcBMI()}) is higher than ${john.fullName}'s (${john.calcBMI()})`)
+    } else {
+        console.log(`${john.fullName}'s  BMI(${john.calcBMI()}) is higher than ${mark.fullName}'s (${mark.calcBMI()})`)
+    }
+}
+
+
+const compare = compareBMI();
+
+console.log(compare)
